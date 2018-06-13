98922f767f2d8bb9fa441e60cdd983717156dff3
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:46:27 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Address/PostAddressHttpTrigger/PostAddressHttpTrigger.cs
+++ NCS.DSS.Address/PostAddressHttpTrigger/PostAddressHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Address.PostAddressHttpTrigger
     public static class PostAddressHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Addresses")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Address))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Addresses")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var address = await req.Content.ReadAsAsync<Models.Address>();
+
+            var addressService = new PostAddressHttpTriggerService();
+            var addressId = addressService.Create(address);
+
+            return addressId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Address record with Id of : " + addressId)
+                };
         }
     }
 }
--- NCS.DSS.Address/PostAddressHttpTrigger/PostAddressHttpTriggerService.cs
+++ NCS.DSS.Address/PostAddressHttpTrigger/PostAddressHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Address.PostAddressHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Address.PostAddressHttpTrigger
 {
     public class PostAddressHttpTriggerService
     {
+        public Guid? Create(Models.Address address)
+        {
+            if (address == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
