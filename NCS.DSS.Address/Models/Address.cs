b8cc2ab925a766a7712b6fadd8c7b6923099320e
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-21 15:41:12 +0100

    Add Regex for Longitude and Latitude

--- NCS.DSS.Address/Models/Address.cs
+++ NCS.DSS.Address/Models/Address.cs
@@ -62,10 +62,12 @@ namespace NCS.DSS.Address.Models
         [Example(Description = "AA11AA")]
         public string AlternativePostCode { get; set; }
 
+        [RegularExpression(@"^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$")]
         [Display(Description = "Geocoded address information")]
         [Example(Description = "-1.50812")]
         public decimal Longitude { get; set; }
 
+        [RegularExpression(@"^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$")]
         [Display(Description = "Geocoded address information")]
         [Example(Description = "52.40100")]
         public decimal Latitude { get; set; }
