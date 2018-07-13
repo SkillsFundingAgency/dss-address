a3493b77818c55832a115116109ca428b95d5070
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-13 09:54:52 +0100

    Remove Length from Address Regex

--- NCS.DSS.Address/Models/Address.cs
+++ NCS.DSS.Address/Models/Address.cs
@@ -6,7 +6,7 @@ namespace NCS.DSS.Address.Models
 {
     public class Address
     {
-        private const string AddressRegEx = @"[A-Za-z0-9 ~!@&amp;'\()*+,\-.\/:;]{1,100}";
+        private const string AddressRegEx = @"[A-Za-z0-9 ~!@&amp;'\()*+,\-.\/:;]";
         private const string PostcodeRegEx = @"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})";
 
         [Display(Description = "Unique identifier for an address.")]
