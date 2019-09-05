90fb466d02d4c34205d6a6d0fb461f20129a2835
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-09-05 09:31:20 +0100

    update tracewriter to ilogger

--- NCS.DSS.Address/AzureSearchDataSyncTrigger/AddressDataSyncTrigger.cs
+++ NCS.DSS.Address/AzureSearchDataSyncTrigger/AddressDataSyncTrigger.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 using NCS.DSS.Address.Helpers;
 using Document = Microsoft.Azure.Documents.Document;
 
@@ -16,19 +17,19 @@ namespace NCS.DSS.Address.AzureSearchDataSyncTrigger
         [FunctionName("SyncAddressDataSyncTrigger")]
         public static async Task Run([CosmosDBTrigger("addresses", "addresses", ConnectionStringSetting = "AddressConnectionString",
                 LeaseCollectionName = "addresses-leases", CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> documents,
-            TraceWriter log)
+            ILogger log)
         {
-            log.Info("Entered SyncDataForCustomerSearchTrigger");
+            log.LogInformation("Entered SyncDataForCustomerSearchTrigger");
 
             SearchHelper.GetSearchServiceClient();
 
-            log.Info("get search service client");
+            log.LogInformation("get search service client");
 
             var indexClient = SearchHelper.GetIndexClient();
 
-            log.Info("get index client");
+            log.LogInformation("get index client");
             
-            log.Info("Documents modified " + documents.Count);
+            log.LogInformation("Documents modified " + documents.Count);
 
             if (documents.Count > 0)
             {
@@ -44,19 +45,19 @@ namespace NCS.DSS.Address.AzureSearchDataSyncTrigger
 
                 try
                 {
-                    log.Info("attempting to merge docs to azure search");
+                    log.LogInformation("attempting to merge docs to azure search");
 
                     await indexClient.Documents.IndexAsync(batch);
 
-                    log.Info("successfully merged docs to azure search");
+                    log.LogInformation("successfully merged docs to azure search");
 
                 }
                 catch (IndexBatchException e)
                 {
-                    log.Error(string.Format("Failed to index some of the documents: {0}",
+                    log.LogError(string.Format("Failed to index some of the documents: {0}",
                         string.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key))));
 
-                    log.Error(e.ToString());
+                    log.LogError(e.ToString());
                 }
             }
         }
